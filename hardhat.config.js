0341c080efc91aa77c47dc4f3fa7c49466ab5e8d
shibacrypt <>
2022-02-07 12:11:42 +0800

    Add hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,4 +8,29 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    testnet: {
+      url: "https://rpc.api.moonbase.moonbeam.network",
+      // url: "https://moonbeam-alpha.api.onfinality.io/public",
+      chainId: 1287,
+      accounts: [process.env.PK],
+    },
+    // moonriver: {
+    //   url: "https://rpc.moonriver.moonbeam.network",
+    //   chainId: 1285,
+    //   accounts: [process.env.PK],
+    // },
+    // moonbeam: {
+    //   url: "https://rpc.api.moonbeam.network",
+    //   chainId: 1284,
+    //   accounts: [process.env.PK],
+    // },
+  },
+  etherscan: {
+    apiKey: {
+      moonbaseAlpha: process.env.MOONSCAN_API_KEY,
+      // moonriver: process.env.MOONSCAN_API_KEY,
+      // moonbeam: process.env.MOONSCAN_API_KEY,
+    }
+  }
 };
